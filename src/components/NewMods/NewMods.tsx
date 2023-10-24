17c4c0346ce0b738c2493d6e0c9776448adec950
USS-Shenzhou <969840795@qq.com>
2023-10-24 15:19:56 +0800

    minor improve

--- src/components/NewMods/NewMods.tsx
+++ src/components/NewMods/NewMods.tsx
@@ -12,6 +12,8 @@ const Mod = ({modUrl, buttons}) => {
             .catch((error) => console.error(error));
     }, [modUrl]);
 
+    const latest = 'Latest: ';
+
     return (
         <div className={styles.modContainer}>
             {modData && (
@@ -28,7 +30,16 @@ const Mod = ({modUrl, buttons}) => {
                             </a>
                         </h2>
                         <p className={styles.summary}>{modData.summary}</p>
-                        <p className={styles.latestVersion}>最新版本: {modData.download.display}</p>
+                        <p className={styles.latestVersion}>
+                            {latest}
+                            <a
+                                href={modData.download.url}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                {modData.download.display}
+                            </a>
+                        </p>
                         <div className={styles.buttonContainer}>
                             {buttons.map((button, index) => (
                                 <a
@@ -65,6 +76,7 @@ const NewMods: React.FC = () => {
             <h2>更新中</h2>
             <Mod modUrl={baseAPIUrl + mp} buttons={[
                 {text: 'All downloads', url: baseCFUrl + mp + '/files'},
+                {text: 'Demo', url: 'https://www.bilibili.com/video/BV1Ao4y1t7XC'},
                 {text: 'Modrinth', url: 'https://modrinth.com/mod/mad-particle'},
                 {text: 'MCMod', url: 'https://www.mcmod.cn/class/7482.html'},
             ]}/>
@@ -74,18 +86,20 @@ const NewMods: React.FC = () => {
             ]}/>
             <Mod modUrl={baseAPIUrl + brighter} buttons={[
                 {text: 'All downloads', url: baseCFUrl + brighter + '/files'},
+                {text: 'Demo', url: 'https://www.bilibili.com/video/BV17Q4y1e7Ri'},
                 {text: 'MCMod', url: 'https://www.mcmod.cn/class/5215.html'},
             ]}/>
             <h2>制作中</h2>
             <Mod modUrl={baseAPIUrl + r6} buttons={[
                 {text: 'All downloads', url: baseCFUrl + r6 + '/files'},
                 {text: 'MCMod', url: 'https://www.mcmod.cn/class/4799.html'},
-                ]}/>
+            ]}/>
             <h2>停更</h2>
             <Mod modUrl={baseAPIUrl + extinguish} buttons={[
                 {text: 'All downloads', url: baseCFUrl + extinguish + '/files'},
+                {text: 'Demo', url: 'https://www.bilibili.com/video/BV1aG411H7kQ'},
                 {text: 'MCMod', url: 'https://www.mcmod.cn/class/6752.html'},
-                ]}/>
+            ]}/>
         </section>
     );
 };
