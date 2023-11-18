8d49b983fb93c2f164b415e845ac1a597ca188cd
USS-Shenzhou <969840795@qq.com>
2023-11-18 16:19:50 +0800

    update Docusaurus to 3.0.0

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,9 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const {themes} = require('prism-react-renderer');
+const lightTheme = themes.github;
+const darkTheme = themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -181,9 +182,9 @@ const config = {
         `,
       },
       prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-        additionalLanguages: ['java','toml','gradle'],
+        theme: lightTheme,
+        darkTheme: darkTheme,
+        additionalLanguages: ['java','toml','gradle','json'],
       },
     }),
     markdown:{
