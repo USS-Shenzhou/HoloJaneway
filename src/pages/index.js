6a8bbf705854ebcec33b23b53821a48595e02fd9
USS-Shenzhzou <969840795@qq.com>
2025-06-02 09:42:53 +0800

    update i18n

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,18 +13,16 @@ function HomepageHeader() {
             <h1 className="hero__title">
                 <Translate>欢迎</Translate>
             </h1>
-            <text style={{
-                "font-size": "1.5rem"
+            <p style={{
+                fontSize: "1.5rem"
             }}>
-                <br/>
                 <Translate>在上方导航栏选择想要查看的项目。</Translate>
-            </text>
-            <text style={{
-                      "font-size": "1.3rem"
+            </p>
+            <p style={{
+                      fontSize: "1.3rem"
                 }}>
-                    <br/>
                     English translations may not cover all pages.
-                </text>
+                </p>
         </div>
     </header>);
 }
@@ -42,11 +40,11 @@ export default function Home() {
                     src={require("@site/static/img/head.png").default}
                     alt="USS_Shenzhou head"
                 />
-                <text className={styles.text}>
+                <p className={styles.text}>
                     <Translate>你好！我是</Translate><strong>USS_Shenzhou</strong><Translate>，你也可以叫我神舟。</Translate>
                     <br/><Translate>我主要使用Forge/NeoForge为Minecraft Java Edition编写模组。</Translate>
                     <br/><Translate>你可以在页面底端找到我的其他赛博刷新点。</Translate>
-                </text>
+                </p>
             </div>
             <ModList></ModList>
         </main>
